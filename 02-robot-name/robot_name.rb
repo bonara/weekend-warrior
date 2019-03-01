153fc5183528bea10b82f2d86e9096b42c15e4d1
Nara <naraa_520@yahoo.com>
2019-03-01 14:31:21 -0800

    Finished the assignment with passing tests.

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,19 @@
+class Robot
+  attr_accessor :name
+  def initialize
+    @name = self.generate
+  end
+
+  def generate
+    chars = ("A".."Z").to_a
+    digits = (0..9).to_a
+    name = chars.sample(2).flatten.join("") + digits.sample(3).flatten.join("")
+    return name
+  end
+
+  def reset
+    @name = self.generate
+  end
+end
+
 
